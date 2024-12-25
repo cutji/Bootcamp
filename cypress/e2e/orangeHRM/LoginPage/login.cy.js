26737986e9b6e2b648e45907342be81c2ce7b4e3
unknown <jihanriska030@gmail.com>
2024-12-25 17:20:24 +0700

    testing with pom

--- cypress/e2e/orangeHRM/LoginPage/login.cy.js
+++ cypress/e2e/orangeHRM/LoginPage/login.cy.js
@@ -1,74 +1,75 @@
 /// <reference types="cypress"/>
+import loginPage from "../../../pom/orangeHRM/Login/login";
 
-describe('Tes Fitur Login di OrangeHRM dengan Intercept', () => {
+describe('Tes Fitur Login di OrangeHRM dengan Intercept dan POM', () => {
 
     // 1.login menggunakan data Username dan Password yang benar
     it('Berhasil login dengan Username dan Password yang benar', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); 
-        cy.get('[class="oxd-text oxd-text--h5 orangehrm-login-title"]').should('have.text','Login');
-        cy.get('[name="username"]').type('Admin');
-        cy.get('[name="password"]').type('admin123');
+        loginPage.textLogin().should('have.text','Login');
+        loginPage.inputusername().type('Admin');
+        loginPage.inputPassword().type('admin123');
         cy.intercept('GET', '**/employees/action-summary').as('actionsummary'); 
-        cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
+        loginPage.buttonLogin().click();
         cy.wait('@actionsummary').its('response.statusCode').should('eq', 200);
-        cy.get('[class="oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module"]').should('have.text', 'Dashboard');
+        loginPage.menuDashboard().should('have.text', 'Dashboard');
     });
 
    // 2.login dengan tidak mengisi Username dan Password
    it('Gagal login tanpa mengisi Username dan Password', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); 
-    cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
+    loginPage.buttonLogin().click();
     cy.url().should('include', '/auth/login'); 
 });
 
 // 3. Pengguna login dengan mengisi Username tidak sesuai format dan tidak mengisi Password
 it('Gagal login dengan Username tidak sesuai format dan Password kosong', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); 
-    cy.get('input[name="username"]').type('admin#@');
-    cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
+    loginPage.inputusername().type('admin#@');
+    loginPage.buttonLogin().click();
     cy.url().should('include', '/auth/login');
 });
 
 // 4. Pengguna login dengan mengisi Username sesuai format dan tidak mengisi Password
 it('Gagal login dengan Username yang sesuai format dan Password kosong', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); 
-    cy.get('input[name="username"]').type('Admin');
-    cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
+    loginPage.inputusername().type('Admin');
+    loginPage.buttonLogin().click();
     cy.url().should('include', '/auth/login');
 });
 
 // 5. Pengguna login dengan tidak mengisi Username dan memasukkan Password yang benar
 it('Gagal login dengan Username kosong dan Password yang benar', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); 
-    cy.get('input[name="password"]').type('admin123');
-    cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
+    loginPage.inputusername().type('admin123');
+    loginPage.buttonLogin().click();
     cy.url().should('include', '/auth/login');
 });
 
 // 6. Pengguna login dengan mengisi Username yang benar dan Password yang salah
 it('Gagal login dengan Username yang benar dan Password yang salah', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); 
-    cy.get('input[name="username"]').type('Admin');
-    cy.get('input[name="password"]').type('admin2322');
-    cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();;
-    cy.get('.oxd-alert-content').should('contain.text', 'Invalid credentials');
+    loginPage.inputusername().type('Admin');
+    loginPage.inputPassword().type('admin2322');
+    loginPage.buttonLogin().click();
+    loginPage.alertInvalidCredentials().should('contain.text', 'Invalid credentials');
 });
 
 // 7. Pengguna login dengan mengisi Username yang salah dan Password yang benar
 it('Gagal login dengan Username yang salah dan Password yang benar', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); 
-    cy.get('input[name="username"]').type('admin3');
-    cy.get('input[name="password"]').type('admin123');
-    cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
-    cy.get('.oxd-alert-content').should('contain.text', 'Invalid credentials');
+    loginPage.inputusername().type('admin3');
+    loginPage.inputPassword().type('admin123');
+    loginPage.buttonLogin().click();
+    loginPage.alertInvalidCredentials().should('contain.text', 'Invalid credentials');
 });
 
 // 8. Pengguna login dengan mengisi Password kurang dari 8 karakter
 it('Gagal login dengan Password kurang dari 8 karakter', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'); 
-    cy.get('input[name="username"]').type('Admin');
-    cy.get('input[name="password"]').type('admin12');
-    cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
-    cy.get('.oxd-alert-content').should('contain.text', 'Invalid credentials');
+    loginPage.inputusername().type('Admin');
+    loginPage.inputPassword().type('admin12');
+    loginPage.buttonLogin().click();
+    loginPage.alertInvalidCredentials().should('contain.text', 'Invalid credentials');
 });
 });
---
+++ cypress/pom/orangeHRM/Login/login.js
@@ -0,0 +1,25 @@
+ export default class loginPage {
+    static textLogin() {
+        return cy.get('[class="oxd-text oxd-text--h5 orangehrm-login-title"]');
+    }
+
+    static inputusername(){
+        return cy.get('[name="username"]');
+    }
+
+    static inputPassword(){
+        return cy.get('[name="password"]');
+    }
+
+    static buttonLogin(){
+        return cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]');
+    }
+
+    static menuDashboard(){
+        return cy.get('[class="oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module"]');
+    }
+
+    static alertInvalidCredentials(){
+        return cy.get('.oxd-alert-content');
+    }
+ }
